6d9df00af4dc3d89c5d3bca2facfcb2bad5d33ee
DanKos22 <G00397054@atu.ie>
2024-09-22 19:29:46 +0300

    added productService class

--- src/main/java/ie/atu/week2_refresher/ProductController.java
+++ src/main/java/ie/atu/week2_refresher/ProductController.java
@@ -1,17 +1,16 @@
 package ie.atu.week2_refresher;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 public class ProductController {
-
+    /*
     private List<Product> productList = new ArrayList<Product>();
 
     @GetMapping("/products")
@@ -24,6 +23,14 @@ public class ProductController {
     public Product addProduct(@RequestBody Product product){
         productList.add(product);
         return product;
+    }
+    */
+
+    private final ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService){
+        this.productService = productService;
     }
 
 }
